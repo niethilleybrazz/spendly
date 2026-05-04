5dd569b3d0accbda9b256ebb43edf4236b172e48
Niethilley Braz <niethilley@gmail.com>
2026-05-04 18:16:53 -0300

    feat(frontend):  adiciona rota de autenticacao e as paginas do dashboard

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -6,13 +6,23 @@ import {
   Navigate,
 } from 'react-router-dom'
 
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import Home from './pages/Dashboard/Home'
+import Income from './pages/Dashboard/Income'
+import Expense from './pages/Dashboard/Expense'
+
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
           <Route path="/" element={<Root />} />
-          <Route path="/login" exact element={<Login/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signUp" element={<SignUp/>} />
+          <Route path="/dashboard" element={<Home/>} />
+          <Route path="/income" element={<Income/>} />
+          <Route path="/expense" element={<Expense/>} />
         </Routes>
       </Router>
     </div>
@@ -20,3 +30,14 @@ const App = () => {
 }
 
 export default App
+
+const Root = () => {
+  // Checa se o token existe no localStorage
+  const isAuthenticated = !!localStorage.getItem("token")
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  )
+}
